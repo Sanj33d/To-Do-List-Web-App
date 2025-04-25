4fad17fd9a103517fce96bea379b1e37d9be415d
Sanj33d <sanjeedsajeen8@gmail.com>
2025-04-25 16:46:44 +0600

    almost done

---
+++ script/script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+const addToTask = () => {
+    if (inputBox.value === "") {
+        alert("Please add a task first");
+    }
+    else{
+        const li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        const span  = document.createElement("span");
+        span.innerHTML = "×";
+        li.appendChild(span);
+        
+    }
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+
+const saveData = () => {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+const showData = () => {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+showData();
